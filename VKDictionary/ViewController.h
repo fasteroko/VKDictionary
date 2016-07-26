7608f510a3a2ff412bb7968f329d7d247b4902d5
Vladyslav <vladyslav@vladyslavs-macbook-pro.local>
2016-07-26 15:42:34 +0300

    Initial Commit

---
+++ VKDictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKDictionary
+//
+//  Created by Vladyslav on 7/26/16.
+//  Copyright © 2016 Vladyslav Korneyev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
